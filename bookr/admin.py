3fdfd7b0d9663554aceeff18ed66e054dadd2579
Dmytro-bit <dziumanenkod@gmail.com>
2022-10-01 12:31:13 +0200

    Chapter 9 Ac.9.1("")

--- bookr/admin.py
+++ bookr/admin.py
@@ -4,4 +4,4 @@ class BookrAdminSite(admin.AdminSite):
     title_header = "Bookr admin"
     site_header = "Bookr administration"
     index_title = "Bookr site admin"
-    site_title =  "Bookr adm"
+    site_title =  "Bookr admin"
